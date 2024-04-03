139764fd29fe9e08588eb3dad7ba73896aba14a2
gaurishankar007 <gaurisharma358@gmail.com>
2024-04-03 16:05:00 +0545

    refactor: removing unnecessary data

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_ddd/firebase_options.dart';
-import 'config/routes/routes.dart';
-import 'config/themes/theme.dart';
-import 'data/remote/repositories/firebase_auth_repo.dart';
-import 'presentation/screens/auth/login_register.dart';
-import 'presentation/screens/home.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,18 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Firebase With DDD',
-      theme: lightTheme,
-      home: StreamBuilder(
-        stream: AuthFirebase().authStateChanges,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const Home();
-          } else {
-            return const LoginRegister();
-          }
-        },
-      ),
-      onGenerateRoute: AppRoute.onGenerated,
+      home: Scaffold(appBar: AppBar(title: const Text("Firebase DDD"))),
     );
   }
 }
